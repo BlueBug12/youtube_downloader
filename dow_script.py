66cdb60912d5bb31e6c191216a2287a4b33c8a41
BlueBug12 <benson.liu.himself@gmail.com>
2019-07-17 22:28:44 +0800

    Improve layout setting New function: 	delete_node

--- dow_script.py
+++ dow_script.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import os
 import threading
 import tkinter as tk
 from tkinter import ttk
-
-#https://yogapan.github.io/2017/08/16/Youtube-dl%E6%BF%83%E7%B8%AE%E6%95%99%E5%AD%B8%E7%AD%86%E8%A8%98/
+from bs4 import BeautifulSoup
+import requests
 
 url_list=[]
+name_list=[]
 def thread_it(func,*args):
 		#mutlithreading
 		print('Number of threads:',end='')
@@ -15,38 +17,60 @@ def thread_it(func,*args):
 		added_t1.start()
 
 def add_node(url):
-    url_list.append(url)
-    e1_var.set('')
+	request=requests.get(url)
+	context = request.content
+	soup=BeautifulSoup(context,"html.parser")
+	video_name = soup.select('h1.watch-title-container')[0].get_text();
+	video_name = video_name.replace('\n','')
+	video_name = video_name.replace(' ','')
+	name_list.append(video_name)
+	url_list.append(url)
+	e1_var.set('')
+	c1['value']=name_list
+	c1.grid(padx=10,column=1,columnspan=3,row=2)
 
-    c1['value']=url_list
-    c1.grid(column=0,columnspan=3,row=2)
+def delete_node(index):
+	del url_list[index]
+	del name_list[index]
+	c1_var.set("                                       (如需刪除再點選)")
+	c1['value']=name_list
+	c1.grid(ipadx=10,column=1,columnspan=3,row=2)
+	
 
-def download():
-    for url in url_list:
-        #os.system("youtube-dl -x --audio-format mp3 --embed-thumbnail  --metadata-from-title\"%(aaaa)s - %(bbbb)s\""+url)
-        os.system("youtube-dl -x --audio-format mp3 --embed-thumbnail --add-metadata "+url)
-        print('done!!') 
+def download(destination):
+	for url in url_list:
+		destination = "\""+destination+"\""
+		os.system("youtube-dl --output /%(title)s.%(ext)s -x --audio-format mp3 "+url)
+		print('done!!') 
+	name_list=[]
+	global url_list
+	url_list=[]
+	c1_var.set("                                       (如需刪除再點選)")
+	c1['value']=[]
+	c1.grid(ipadx=10,column=1,columnspan=3,row=2)
 
 
 root=tk.Tk()
 root.title('Welcome to Youtube Downloader')
-root.geometry('400x100')
-tk.Label(root,text="請貼上網址").grid(column=0,row=0)
+root.geometry('650x200')
+tk.Label(root,text="請貼上網址: ").grid(column=0,row=1)
+tk.Label(root,text="下載列表:   ").grid(column=0,row=2)
 
 e1_var=tk.StringVar()
 e1_var.set('')
 e1=tk.Entry(root,width=50,textvariable=e1_var)
-e1.grid(column=0,columnspan=3,row=1)
+e1.grid(ipadx=10,column=1,columnspan=3,row=1)
 
-tk.Button(root,text="Add",command=lambda :add_node(e1.get())).grid(column=3,row=1)
+tk.Button(root,text="加入",command=lambda :add_node(e1.get())).grid(column=4,row=1)
 
 c1_var=tk.StringVar()
+c1_var.set("                                       (如需刪除再點選)")
 c1=ttk.Combobox(root,width=50,textvariable=c1_var,state='readonly')
-c1['value']=url_list
-c1.grid(column=0,columnspan=3,row=2)
+c1['value']=name_list
+c1.grid(padx=10,column=1,columnspan=3,row=2)
 
-tk.Button(root,text="Start Download",command=lambda :thread_it(download)).grid(column=1,row=3)
+tk.Button(root,text="刪除",command=lambda :delete_node(c1.current())).grid(column=4,row=2)
 
-
+tk.Button(root,text="開始下載",command=lambda :thread_it(download("C:/Users/User/Desktop"))).grid(column=2,row=3)
 
 root.mainloop()
